574b0b2c7027f44fad19398d24c582d004899715
Hanes Oliveira <hanesoliveira@98247598273489.local>
2022-09-26 14:15:40 -0600

    Fix. Changed the name for function that creates a new multivector.

--- multivector.go
+++ multivector.go
@@ -14,10 +14,10 @@ type Multivector struct {
 	E123 *big.Rat
 }
 
-// NewMultivector creates a new multivector where all coefficients are rational numbers.
+// New creates a new multivector where all coefficients are rational numbers.
 // Since the package already informs the dimension (i.e., 3d) we do not need to reinforce
 // in the function name.
-func NewMultivector(e0, e1, e2, e3, e12, e13, e23, e123 big.Rat) Multivector {
+func New(e0, e1, e2, e3, e12, e13, e23, e123 big.Rat) Multivector {
 	return Multivector{
 		E0:   &e0,
 		E1:   &e1,
