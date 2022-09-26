58c9ac34229a2db01b1e193109573cbef50e2a9d
Hanes Oliveira <hanesoliveira@98247598273489.local>
2022-09-26 14:21:21 -0600

    WIP. Package documentation initial definition.

---
+++ doc.go
@@ -0,0 +1,11 @@
+// Package ga3d includes functions to create and manipulate multivectors in the 3rd dimension.
+// Geometric product.
+// Addition.
+// Subtraction.
+// Scalar multiplication.
+// Scalar division.
+// Involution (Clifford Conjugation).
+// Inverse.
+// Rationalize.
+
+package ga3d
