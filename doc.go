39b3c6f807d126d32960daadbd50dbbc3dddd2b1
Hanes Oliveira <hanesoliveira@98247598273489.local>
2022-09-28 09:54:24 -0600

    WIP. Updated dependencies of functions in the documentation for the package. Still needs to be organized later.

--- doc.go
+++ doc.go
@@ -1,11 +1,23 @@
 // Package ga3d includes functions to create and manipulate multivectors in the 3rd dimension.
 // Geometric product.
-// Addition.
-// Subtraction.
+// Addition / Subtraction.
 // Scalar multiplication.
 // Scalar division.
-// Involution (Clifford Conjugation).
-// Inverse.
+// Clifford Conjugation (Involution).
+// Amplitude squared.
+//   *Clifford conjugation.
+//   *Geometric product.
+// Reverse.
 // Rationalize.
+//   *Amplitude squared.
+//   *Reverse.
+//   *Geometric product.
+// Inverse.
+//   *Scalar division.
+//   *Amplitude squared.
+//   *Reverse.
+//   *Clifford conjugation.
+//   *Geometric product.
+//   *Rationalize.
 
 package ga3d
